f84e9aea6163a8b69d2679eef0baaefe354c829a
Vijay Iyer <vijayiyer97@gmail.com>
2020-07-25 09:53:22 -0400

    project creation
    project was created using SwiftUI 2.0 and a complete SwiftUI app lifecycle

---
+++ Sudogu/SudoguApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudoguApp.swift
+//  Sudogu
+//
+//  Created by Vijay Iyer on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct SudoguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
